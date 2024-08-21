5ada7f5aabf4889bf5a6198f40b4bc23ee9c5950
Angeliuxxxx <amayazumayaangel@gmail.com>
2024-08-20 20:43:14 -0600

    Add files via upload
    programa 1

---
+++ programa 1.py
@@ -0,0 +1,9 @@
+entrada = input("Ingresa caracteres: ")
+if entrada.isalpha():
+    print("Es una palabra")
+elif entrada.isdigit():
+    print("Es un numero")
+elif any(char.isdigit() for char in entrada) and any(char.isalpha() for char in entrada):
+    print("Es compuesta")
+else:
+    print("Cadena no valida")
